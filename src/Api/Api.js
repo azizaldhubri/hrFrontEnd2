50c07b484d95c6fca8752fbacc508c115f7f8024
azizaldhubri <azizaldhubri@gmail.com>
2025-05-15 03:25:29 +0300

    Update Api.js

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,4 +1,5 @@
 // export const baseUrl=`http://127.0.0.1:8000/api`;
+ 
 export const baseUrl=`https://backend1hr-production1.up.railway.app/api`;
 // export const baseUrl=`https://free-001.yemenhosting.com/api`;
 // export const baseUrl=`https://backend1hr-production1.up.railway.app/api`;
